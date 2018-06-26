67426655f05e57c19eaaf465e9bf6733ec466f82
Jason <jason@meetmycampus.com>
2018-06-26 15:49:42 -0400

    ud

--- inc/functions.php
+++ inc/functions.php
@@ -1456,8 +1456,9 @@ function get_community($communityId,$collegeId){
 			$stmt = $connect->prepare("SELECT communities.community_id, communities.category_id, categories.category,college_student.userName,community_name,community_message,community_category,community_description,community_type,community_color,date_created  FROM communities 
 										INNER JOIN college_student ON communities.creator_id = college_student.id
 	                                    INNER JOIN categories ON communities.category_id =  categories.category_id
-										WHERE community_id = ?");
+										WHERE community_id = ? AND communities.college_id = ? ");
 			$stmt->bindParam(1,$communityId,PDO::PARAM_INT);
+			$stmt->bindParam(2,$collegeId,PDO::PARAM_INT);
 			$stmt->execute();
 			$connect->commit();
 			return $stmt->fetch(PDO::FETCH_ASSOC);
